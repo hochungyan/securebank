a1621418c27e758d58beb03c234c7922a24af744
hochungyan <hochungyan@live.hk>
2026-02-15 23:14:08 +0800

    day1

--- src/main/java/com/securebank/Main.java
+++ src/main/java/com/securebank/Main.java
@@ -4,5 +4,15 @@ public class Main {
     public static void main(String[] args){
         System.out.println("SecureBank v0.1");
         System.out.println("Java version: " + System.getProperty("java.version"));
+
+        //Declare Variables with explicit types
+        String bankName = "SecureBank";
+        int totalAccounts = 0;
+        double interestRate = 0.025;
+        boolean isOpen = true;
+
+        System.out.println(bankName + " is " + (isOpen ? "open" : "closed"));
+        System.out.println("Interest rate: " + (interestRate * 100) + "%");
+
     }
 }
