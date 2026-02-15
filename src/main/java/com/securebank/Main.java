cbccaf728630715b03306a46375374ea2db6be88
hochungyan <hochungyan@live.hk>
2026-02-15 22:59:56 +0800

    first commit

---
+++ src/main/java/com/securebank/Main.java
@@ -0,0 +1,8 @@
+package com.securebank;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("SecureBank v0.1");
+        System.out.println("Java version: " + System.getProperty("java.version"));
+    }
+}
